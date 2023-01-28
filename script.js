813bc1100d5b77421b43069c10acfc696d86167d
ompatil19 <ompatil2002@gmail.com>
2023-01-29 02:24:47 +0530

    Progress after the formation of initial web page

---
+++ script.js
@@ -0,0 +1,6 @@
+let typed=new Typed( ".auto-input", {
+    strings: ["Welcome to the books store","Read all the books you want","Buy share read"],
+    typeSpeed :50, 
+    backSpeed:50, 
+    loop:true
+})
